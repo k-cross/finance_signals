ad776fb2d7e66ac8a174ee5f095d596a15039d57
hansenwu <hansen.wu.85@gmail.com>
2016-11-30 19:22:34 -0800

    Data is now in dictionary form, graphed at end of program, technicals calculated, rudimentary predictions

--- quant/quantpredict.py
+++ quant/quantpredict.py
@@ -21,10 +21,92 @@ predicted future movement
 Powered by magic and digital foxes
 """
 
+import numpy as np
+import talib.abstract as ta
+# import talib.abstract as tech
 
-def analyze(history):
+
+# sma = abstract.SMA
+# bband = abstract.BBANDS
+# rsi = abstract.RSI
+
+
+class profile(object):
     """
-    Documentation pending
+    General info about a ticker. 'Studies' is synonymous with 'technical indicators'.
     """
-    # Placeholder print out first line of input
-    print(history[0])
+
+    def __init__(self, ticker, pricing):
+        self.ticker = ticker
+        self.pricing = pricing
+        # self.studies = {}
+
+    def load(self):
+        """ Load previous processed results """
+        with open(self.ticker + ".csv") as file:
+            self.pricing = file.read()
+
+    def save(self):
+        """ Save processed results """
+        with open(self.ticker + ".csv") as file:
+            # TODO: save as proper csv
+            file.write(self.pricing)
+
+    def add_study(self, study_name, lst):
+        """ Adds study data points to profile """
+        # self.studies[study_name] = lst
+        # print(len(self.pricing))
+        # print(len(lst))
+        for index, date in enumerate(self.pricing):
+            date[study_name] = lst[index]
+
+    def getplot(self, plot):
+        return [date[plot] for date in self.pricing]
+
+
+def analyze(profile):
+    """
+    Compute studies and draw conclusions about stock's performance
+    """
+    # Update profile with technical indicators
+    technical(profile)
+    # Parse and draw conclusions from technical data
+
+
+def technical(profile):
+    """
+    Updates profile with SMA, BB, and RSI calculations
+    In: profile with relevant data
+    """
+    prices = {'open': np.array([record['o'] for record in profile.pricing]),
+              'high': np.array([record['h'] for record in profile.pricing]),
+              'low': np.array([record['l'] for record in profile.pricing]),
+              'close': np.array([record['c'] for record in profile.pricing]),
+              'volume': np.array([record['vol'] for record in profile.pricing])}
+    # print(prices['open'])
+    # print(prices)
+
+    sma = ta.SMA(prices, timeperiod=200)
+    profile.add_study("sma", [round(float(point), 2) for point in sma])
+
+    bb_upper, bb_middle, bb_lower = ta.BBANDS(prices, 100, 2, 2)
+    profile.add_study("bb_upper", [round(float(point), 2) for point in bb_upper])
+    profile.add_study("bb_middle", [round(float(point), 2) for point in bb_middle])
+    profile.add_study("bb_lower", [round(float(point), 2) for point in bb_lower])
+
+    rsi = ta.RSI(prices, timeperiod=14)
+    profile.add_study("rsi", [round(float(point), 2) for point in rsi])
+
+
+def predict(profile):
+    """
+    Weighs different indicators from an analyzed profile, and deduce a prediction on future movement
+    """
+    today = len(profile.pricing) - 1
+    data = profile.pricing[today]
+    # print(todaysprice)
+    # momentum = [data['bb_middle'], data['sma']]
+    if data['c'] > data['bb_middle'] and data['c'] > data['sma']:
+        print("Strong!")
+    else:
+        print("Don't buy!")
--- quant/timemachine.py
+++ quant/timemachine.py
@@ -15,19 +15,21 @@ from datetime import datetime
 import quantpredict
 import bisect
 import copy
+import matplotlib.pyplot as grapher
+# import _tkinter
 # import csv
 
 # Static Data
 ticker = "AMZN"
-csv = "../samples/%s.csv" % ticker
+samplespath = "../samples/%s.csv" % ticker
 lines_5yr = 1260
-history = []
+cumulativehistory = []
 
 
 def jumpTo(year, month, day):
     """ Find index of a requested day in historic data """
     d = datetime(year, month, day).date()
-    return bisect.bisect_left([row[0] for row in history], d)
+    return bisect.bisect_left([row['date'] for row in cumulativehistory], d)
 
 
 # Read history
@@ -35,37 +37,47 @@ def jumpTo(year, month, day):
 # Consider using dictionary if it helps with quantpredict so you don't need indexes all the time or something
 # https://docs.python.org/3/library/csv.html
 line_counter = 0
-with open(csv) as file:
+with open(samplespath) as file:
     file.readline()  # remove csv header
     for line in file:
         line_counter += 1
         line = line.strip('\n')
         data = line.split(',')
-
-        day = datetime.strptime(data[0], '%Y-%m-%d').date()
-        # day = str(day)  # If date matters, remove str conversion
-        op = round(float(data[1]), 2)
-        hi = round(float(data[2]), 2)
-        lo = round(float(data[3]), 2)
-        cl = round(float(data[4]), 2)
-        vol = int(data[5])
-
-        history.insert(0, (day, op, hi, lo, cl, vol))
+        d = {'date': datetime.strptime(data[0], '%Y-%m-%d').date(),
+             'o': round(float(data[1]), 2),
+             'h': round(float(data[2]), 2),
+             'l': round(float(data[3]), 2),
+             'c': round(float(data[4]), 2),
+             'vol': int(data[5])}
+        cumulativehistory.insert(0, d)
 
 # Debug stuff
 # print("read %i lines" % line_counter)
-# for x in range(1, 10):
-#     print(history[x])
-# print(history[200])
-# print(history[jumpTo(2011, 7, 1)])
+# for x in range(0, 9):
+#     print(cumulativehistory[x])
+# print(cumulativehistory[200])
+# print(cumulativehistory[jumpTo(2011, 7, 1)])
 
 # Analyze and make prediction
 
-snapshot = copy.deepcopy(history[:jumpTo(2011, 7, 1)])
+# Simulate today is july 1, 2011
+snapshot = copy.deepcopy(cumulativehistory[:jumpTo(2012, 7, 1) + 1])
+q = quantpredict.profile(ticker, snapshot)
+quantpredict.analyze(q)
+quantpredict.predict(q)
 
-# Consider doing some OOP goodness with this:
-# q = quantpredict(snapshot)
-# q.analyze()
-# q.predict()
-# or something
-quantpredict.analyze(snapshot)
+# Sample plotting
+
+# print(q.priceplot())
+# grapher.subplot(211)
+grapher.plot(q.getplot('c'))
+grapher.plot(q.getplot('sma'))
+grapher.plot(q.getplot('bb_upper'))
+grapher.plot(q.getplot('bb_middle'))
+grapher.plot(q.getplot('bb_lower'))
+grapher.ylabel('price')
+
+# grapher.subplot(212)
+# grapher.plot(q.studies['rsi'])
+
+grapher.show()
